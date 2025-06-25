5bc1147a75fc62a1496ffb2b895c8a5422c42621
Sasindu Malhara <sasindumalhara@gmail.com>
2025-06-25 10:12:27 +0530

    Backend init

--- Frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"KEQ3MqW8oCd3KklEoy5fMcHj1k2QXe0dKj11GjL9Mfc=\"\n}"
--- Frontend/.next/static/chunks/src_app_login_page_tsx_ef84c14e._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_7f96f8f4._.js",
-  "static/chunks/src_components_8f52d67f._.js"
-],
-    source: "dynamic"
-});
